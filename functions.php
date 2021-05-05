bc2ec641317ac8390b95eb8c96d2adb9167352aa
Rae Kolke <sheilakolke@gmail.com>
2021-05-05 04:59:51 +0000

    forgot to add

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?
+/*
+require 'database-config.php';
+
+//recieve and process $_GET data
+
+//get requested id
+$requestedID = $_REQUEST["id"];
+$requestedID = htmlspecialchars($requestedID);
+$requestedID = filter_var($requestedID, FILTER_SANITIZE_STRING);
+
+//get requested list
+$requestedList = $_REQUEST["list"];
+$requestedList = htmlspecialchars($requestedList);
+$requestedList = filter_var($requestedList, FILTER_SANITIZE_STRING);
+
+//get information from database
+$sql = "SELECT * FROM $databaseTable
+WHERE name='$situation'
+ORDER BY id";
+$requestedOutput = $db->query($sql);
+if (!$requestedOutput) die("List Error: " . $sql . "<br>" . $db->error);
+
+//converting into JSON
+$requestedJSON = "0";
+
+if ($requestedOutput->num_rows > 0) {
+$outputArray = [];
+while( $row = $requestedOutput->fetch_assoc() ) {
+$outputArray[] = $row;
+}
+$requestedJSON = json_encode($outputArray);
+}
+
+//output JSON
+echo $requestedJSON;
+
+*/
---
+++ scripts.js
@@ -0,0 +1,42 @@
+/*
+// generic AJAX function to load fromFile into object with ID whereTo
+function loadFileInto(fromIdentifier, fromList) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+  
+  // define the fromFile value based on PHP URL
+  fromFile = "functions.php?id=" + fromIdentifier + "&list=" + fromList;
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// prepares code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+		
+			if ((this.readyState == 4) && (this.status == 200)) {
+				
+        console.log("AJAX JSON response: " + this.responseText);
+        
+        //convert JSON from PHP into JS array
+        responseArray = JSON.parse(this.responseText);
+        responseHTML = "";
+        for (x=0; x<responseArray.length; x++) {
+          responseHTML += "<li>" + responseArray[x].content + "</li>";
+        }
+        
+        whereTo = "#" + fromList + " ul";
+        if (fromList == "directions") whereTo = "#" + fromList + " ol";
+        document.querySelector(whereTo).innerHTML = responseHTML;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+				
+			}
+		
+	} // end ajax.onreadystatechange
+
+	// now that everything is set, initiate request
+	ajax.send();
+
+} */
