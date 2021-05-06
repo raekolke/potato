b61c8997418c51db25827906b44e43a98e3f4ae2
Rae Kolke <sheilakolke@gmail.com>
2021-05-06 23:52:19 +0000

    making scripts.js and functions.php pretty with comments

--- functions.php
+++ functions.php
@@ -1,32 +1,26 @@
 <?
+// retrieving database information
 require 'database-config.php';
 
-//recieve and process $_GET data
-
-// what are these requested things getting the information from? are they actually doing anything?
-
-//get requested id
+// get requested id
 $requestedID = $_REQUEST["id"];
 $requestedID = htmlspecialchars($requestedID);
 $requestedID = filter_var($requestedID, FILTER_SANITIZE_STRING);
 
-//get requested list
+// get requested table
 $requestedTable = $_REQUEST["table"];
 $requestedTable = htmlspecialchars($requestedTable);
 $requestedTable = filter_var($requestedTable, FILTER_SANITIZE_STRING);
 
-//get information from database
+// get id and table information from database
 $sql = "SELECT * FROM $requestedTable
 WHERE id=$requestedID
 ORDER BY id";
 $requestedOutput = $db->query($sql);
 if (!$requestedOutput) die("List Error: " . $sql . "<br>" . $db->error);
 
-//converting into JSON
+// converting into JSON
 $requestedJSON = "0";
-
-
-// I think this needs to be changed but I do not understnad what any of this means
 
 if ($requestedOutput->num_rows > 0) {
 
@@ -34,6 +28,6 @@ if ($requestedOutput->num_rows > 0) {
 
 }
 
-//output JSON
+// output JSON
 echo $requestedJSON;
 
