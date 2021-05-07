766049349de7c432c9d678d5cfda5041af849577
Rae Kolke <sheilakolke@gmail.com>
2021-05-07 01:20:58 +0000

    we tried

--- index.php
+++ index.php
@@ -20,47 +20,45 @@
 	<!-- external and internal JavaScript -->
 	<script type="text/javascript" src="scripts.js" defer>
     
-function setCookie(cname,cvalue,exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  var expires = "expires=" + d.toGMTString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+      function setCookie(cname,cvalue,progress,exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        progress = trackProgress;
+        var expires = "expires=" + d.toGMTString();
+        document.cookie = cname + "=" + cvalue + progress + ";" + expires + ";path=/";
+      }
 
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
+      function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+      }
 
-function checkCookie() {
-  var user=getCookie("username");
-  if (user != "") {
-    alert("Welcome again " + user);
-  } else {
-     user = prompt("Please enter your name:","");
-     if (user != "" && user != null) {
-       setCookie("username", user, 30);
-     }
-  }
-}
+      function checkCookie() {
+        var user=getCookie("username");
+        if (user != "") {
+          alert("Welcome again " + user);
+        } else {
+           user = prompt("Please enter your name:","");
+           if (user != "" && user != null) {
+             setCookie("username", user, 30);
+           }
+        }
+      }
 	</script>
   
-  <html>
-
-
-
 </head>
+  
 <body onload="checkCookie()">
   
 <!-- this is the set up for the question_table, but we want the the results of our second table outcome_table to be set up differently because there are not as many variables -->
